c09cf84649a380b0cbe7a0c7a1047292cd4144ae
Sahar-DataScience <sahar.ben.mabrouk.pro@gmail.com>
2024-02-23 19:08:11 +0100

    fix bug

--- scrapper.py
+++ scrapper.py
@@ -6,15 +6,15 @@ import mysql.connector
 #scrapper inputs
 page_name = "AIatMeta"
 posts_count = 5
-browser = "firefox"
+browser = "chrome"
 #proxy = "IP:PORT" #if proxy requires authentication then user:password@IP:PORT
 proxy = None
 timeout = 60 #600 seconds
 headless = False
 #DB credentials
 host_name = 'localhost'
-user_name = 'root'
-password_db = 'ubuntu'
+user_name = 'ubuntu'
+password_db = 'password'
 meta_ai = Facebook_scraper(page_name, posts_count, browser, proxy=proxy, timeout=timeout, headless=headless)
 
 json_data = meta_ai.scrap_to_json()
@@ -31,7 +31,7 @@ scrapped_db = mysql.connector.connect(
 mycursor = scrapped_db.cursor()
 
 
-sql = "CREATE DATABASE facebook_db"  # Replace "mydatabase" with your desired database name
+sql = "CREATE DATABASE IF NOT EXISTS facebook_db"  # Replace "mydatabase" with your desired database name
 mycursor.execute(sql)
 
 
@@ -48,16 +48,17 @@ sql = "CREATE TABLE IF NOT EXISTS  fb_page \
     page_name VARCHAR(255), \
     shares_count INT DEFAULT NULL, \
     comments_count INT DEFAULT NULL, \
-    post_content VARCHAR(255), \
-    post_date DATE NOT NULL)"  # Replace with your desired table and column structure
+    post_content TXT, \
+    post_date DATE, \
+    PRIMARY KEY (id))"  # Replace with your desired table and column structure
 mycursor.execute(sql)
 
 for item_ in data_dict:
-    page_name = item_['name']
-    shares_count = item_['shares']
-    comments_count = item_['comments']
-    post_content = item_['content']
-    post_date = item_['posted_on']
+    page_name = data_dict[item_]['name']
+    shares_count = data_dict[item_]['shares']
+    comments_count = data_dict[item_]['comments']
+    post_content = data_dict[item_]['content']
+    post_date = data_dict[item_]['posted_on']
     
     sql = "INSERT INTO fb_page (page_name, shares_count, comments_count, post_content, post_date) VALUES (%s, %s, %s, %s, %s)"
     values = [
